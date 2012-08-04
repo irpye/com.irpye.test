8bed3ecc0b80f9ce54305bd7d49e7ee7bd53fcf9
irpye <irpyebak@gmail.com>
2012-08-04 15:15:55 +0300

    Update com.irpye.test/src/FirstTest.java

--- com.irpye.test/src/FirstTest.java
+++ com.irpye.test/src/FirstTest.java
@@ -1,4 +1,3 @@
-
 public class FirstTest {
 
 	/**
@@ -6,7 +5,7 @@ public class FirstTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git");
+		System.out.println("Hello Git mod02");
 		
 
 	}
